1050437ebcf23154d26be0dfbc3e4f3f8f5dd436
Constance Baco <constancebasco@gmail.com>
2023-08-02 20:29:50 -0400

    created project model Projects

--- tracker/settings.py
+++ tracker/settings.py
@@ -33,6 +33,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "accounts.apps.AccountsConfig",
+    "projects.apps.ProjectsConfig",
+    "tasks.apps.TasksConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
