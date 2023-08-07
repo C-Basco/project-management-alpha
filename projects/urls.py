b090723833d5708c29a6f8106329db9e0dba958a
Constance Baco <constancebasco@gmail.com>
2023-08-07 11:39:19 -0400

    added a login decorator to projects view

--- projects/urls.py
+++ projects/urls.py
@@ -2,4 +2,6 @@ from django.urls import path, include
 from projects.views import list_projects
 
 
-urlpatterns = [path("", list_projects, name="list_projects")]
+urlpatterns = [
+    path("", list_projects, name="list_projects"),
+]
