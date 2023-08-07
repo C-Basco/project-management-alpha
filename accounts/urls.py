a06f172dd3c5fd401982d2e61c6cdcecd785e6fd
Constance Baco <constancebasco@gmail.com>
2023-08-07 12:21:39 -0400

    Created logout views and path in urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from accounts.views import signup, user_login
+from accounts.views import signup, user_login, user_logout
 
 urlpatterns = [
     path("login/", user_login, name="login"),
     path("signup/", signup, name="signup"),
+    path("logout/", user_logout, name="logout"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -6,6 +6,6 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 @login_required
 def list_projects(request):
-    project = Project.objects.all()
+    project = Project.objects.filter(owner=request.user)
     context = {"project_obj": project}
     return render(request, "projects/list.html", context)
