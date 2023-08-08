d36adf9d8179461c32420f2ae0600b751b862188
Constance Baco <constancebasco@gmail.com>
2023-08-08 01:33:16 -0400

    Developed a show_my_tasks ListView, path and template

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path, include
-from tasks.views import create_task
+from django.urls import path
+from tasks.views import create_task, show_my_tasks
 
 urlpatterns = [
+    path("mine/", show_my_tasks.as_view(), name="show_my_tasks"),
     path("create/", create_task, name="create_task"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.views.generic.list import ListView
 from tasks.forms import TaskForm
+from tasks.models import Task
 
 
 # Create your views here.
@@ -8,7 +10,7 @@ from tasks.forms import TaskForm
 def create_task(request):
     if request.method == "POST":
         form = TaskForm(request.POST)
-        if form.is_valid:
+        if form.is_valid():
             form.save()
             return redirect("list_projects")
     else:
@@ -16,3 +18,12 @@ def create_task(request):
 
     context = {"form": form}
     return render(request, "tasks/create.html", context)
+
+
+class show_my_tasks(ListView):
+    model = Task
+    template_name = "tasks/mine.html"
+    context_object_name = "tasks"
+
+    def get_queryset(self):
+        return Task.objects.filter(assignee=self.request.user)
